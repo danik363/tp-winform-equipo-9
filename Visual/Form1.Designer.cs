f5e5d6463f25ca0b7cbbcfea103fa54bc0f2ea0f
braian-valor <braian.valor@alumnos.frgp.utn.edu.ar>
2023-05-02 17:45:41 -0300

    Agregado de clases Articulo y ArticuloNegocio para el modelado y conexion a la base de datos

--- Visual/Form1.Designer.cs
+++ Visual/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Visual
 {
-    partial class Form1
+    partial class frmPrincipal
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
             this.dgvArticulos = new System.Windows.Forms.DataGridView();
-            this.pcbArticulo = new System.Windows.Forms.PictureBox();
+            this.pboxArticulos = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.tsbAgregar = new System.Windows.Forms.ToolStripButton();
@@ -38,39 +38,44 @@
             this.tsbModificar = new System.Windows.Forms.ToolStripButton();
             this.tsbBuscar = new System.Windows.Forms.ToolStripButton();
             ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pcbArticulo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxArticulos)).BeginInit();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvArticulos
             // 
             this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvArticulos.Location = new System.Drawing.Point(12, 135);
+            this.dgvArticulos.Location = new System.Drawing.Point(26, 157);
+            this.dgvArticulos.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dgvArticulos.Name = "dgvArticulos";
-            this.dgvArticulos.Size = new System.Drawing.Size(601, 375);
+            this.dgvArticulos.RowHeadersWidth = 62;
+            this.dgvArticulos.Size = new System.Drawing.Size(718, 405);
             this.dgvArticulos.TabIndex = 1;
             // 
-            // pcbArticulo
+            // pboxArticulos
             // 
-            this.pcbArticulo.Location = new System.Drawing.Point(698, 135);
-            this.pcbArticulo.Name = "pcbArticulo";
-            this.pcbArticulo.Size = new System.Drawing.Size(396, 375);
-            this.pcbArticulo.TabIndex = 2;
-            this.pcbArticulo.TabStop = false;
+            this.pboxArticulos.Location = new System.Drawing.Point(763, 157);
+            this.pboxArticulos.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pboxArticulos.Name = "pboxArticulos";
+            this.pboxArticulos.Size = new System.Drawing.Size(432, 405);
+            this.pboxArticulos.TabIndex = 2;
+            this.pboxArticulos.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 58);
+            this.label1.Location = new System.Drawing.Point(18, 89);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(268, 31);
+            this.label1.Size = new System.Drawing.Size(398, 47);
             this.label1.TabIndex = 3;
             this.label1.Text = "Listado de articulos";
             // 
             // toolStrip1
             // 
             this.toolStrip1.BackColor = System.Drawing.Color.SlateBlue;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsbAgregar,
             this.tsbEliminar,
@@ -78,7 +83,8 @@
             this.tsbBuscar});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(1106, 25);
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 3, 0);
+            this.toolStrip1.Size = new System.Drawing.Size(1208, 34);
             this.toolStrip1.TabIndex = 4;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -87,7 +93,7 @@
             this.tsbAgregar.Image = global::Visual.Properties.Resources.agregar;
             this.tsbAgregar.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.tsbAgregar.Name = "tsbAgregar";
-            this.tsbAgregar.Size = new System.Drawing.Size(69, 22);
+            this.tsbAgregar.Size = new System.Drawing.Size(104, 29);
             this.tsbAgregar.Text = "Agregar";
             // 
             // tsbEliminar
@@ -95,7 +101,7 @@
             this.tsbEliminar.Image = ((System.Drawing.Image)(resources.GetObject("tsbEliminar.Image")));
             this.tsbEliminar.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.tsbEliminar.Name = "tsbEliminar";
-            this.tsbEliminar.Size = new System.Drawing.Size(70, 22);
+            this.tsbEliminar.Size = new System.Drawing.Size(102, 29);
             this.tsbEliminar.Text = "Eliminar";
             // 
             // tsbModificar
@@ -104,7 +110,7 @@
             this.tsbModificar.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.tsbModificar.Name = "tsbModificar";
             this.tsbModificar.RightToLeftAutoMirrorImage = true;
-            this.tsbModificar.Size = new System.Drawing.Size(78, 22);
+            this.tsbModificar.Size = new System.Drawing.Size(115, 29);
             this.tsbModificar.Text = "Modificar";
             // 
             // tsbBuscar
@@ -112,26 +118,27 @@
             this.tsbBuscar.Image = ((System.Drawing.Image)(resources.GetObject("tsbBuscar.Image")));
             this.tsbBuscar.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.tsbBuscar.Name = "tsbBuscar";
-            this.tsbBuscar.Size = new System.Drawing.Size(62, 22);
+            this.tsbBuscar.Size = new System.Drawing.Size(91, 29);
             this.tsbBuscar.Text = "Buscar";
             this.tsbBuscar.ToolTipText = "Modificar\r\n";
             // 
-            // Form1
+            // frmPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSlateBlue;
-            this.ClientSize = new System.Drawing.Size(1106, 538);
+            this.ClientSize = new System.Drawing.Size(1208, 705);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.pcbArticulo);
+            this.Controls.Add(this.pboxArticulos);
             this.Controls.Add(this.dgvArticulos);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmPrincipal";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pcbArticulo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxArticulos)).EndInit();
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -141,7 +148,7 @@
 
         #endregion
         private System.Windows.Forms.DataGridView dgvArticulos;
-        private System.Windows.Forms.PictureBox pcbArticulo;
+        private System.Windows.Forms.PictureBox pboxArticulos;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripButton tsbAgregar;
--- Visual/Form1.cs
+++ Visual/Form1.cs
@@ -10,16 +10,17 @@ using System.Windows.Forms;
 
 namespace Visual
 {
-    public partial class Form1 : Form
+    public partial class frmPrincipal : Form
     {
-        public Form1()
+        public frmPrincipal()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            dgvArticulos.DataSource = negocio.listar();
         }
     }
 }
